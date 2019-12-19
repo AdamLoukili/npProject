f552fadd776acb67a9cb16906b039cacd8bf2ba6
Adam_Loukili <ad.loukili@outlook.fr>
2019-12-19 16:31:22 +0100

    save 19-12-2019

--- gallerie.php
+++ gallerie.php
@@ -1,19 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+  session_start();
+  include 'header.php';
+?>
+    <section class="gallerieAll">     
+      <section class="titreGalerie">
+        <hr class="lignegal1">
+        <p class="gal">Ajouté récement</p>
+        <hr class=lignegal2>
+      </section> 
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="style.css">
-  <title>Document</title>
-</head>
+      <section class="gallerieRecent">
+        <ul>
+          <li><img src="imgs/galerie/recent1.jpg" class="imgGalrecent" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/recent2.png" class="imgGalrecent" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/recent3.png" class="imgGalrecent" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/recent4.jpg" class="imgGalrecent" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/recent5.png" class="imgGalrecent" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/recent6.jpg" class="imgGalrecent" alt="imgGalerie" onclick="grandir()"></li>
+        </ul>
+      </section>
 
-<body>
-    <?php include 'header.php'; ?>
+      <section class="titreGalerie">
+        <hr class="lignegal1">
+        <p class="gal">Wallpapers</p>
+        <hr class=lignegal2>
+      </section>
+
+      <section class="gallerieTout">
+        <ul>
+          <li><img src="imgs/galerie/gal1.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/gal2.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+        </ul>
+        <ul>
+          <li><img src="imgs/galerie/gal3.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/gal4.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+        </ul>
+        <ul>
+          <li><img src="imgs/galerie/gal5.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/gal6.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+        </ul>
+        <ul>
+          <li><img src="imgs/galerie/gal7.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+          <li><img src="imgs/galerie/gal8.jpg" class="imgGal" alt="imgGalerie" onclick="grandir()"></li>
+        </ul>
+      </section>
+    </section>
 
      <!-- ---------------------- This Is Footer Part -------------------- -->
      <?php include 'footer.php'; ?>
-    <script src="https://kit.fontawesome.com/75bed6266a.js"></script>
-    <script scr="js.js"></script>
 </body>
